24676968f7ce44ae837bf7637c856e1e48cce30a
Moses Mutisya <mutisyamoses722@gmail.com>
2024-03-18 13:24:32 +0300

    working with strings

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+// const currentUser = "Grace Hopper";
+// let welcomeMessage = "Welcome to Flatbook,";
+// welcomeMessage = "Welcome to Flatbook, ${currentUser}";
+// welcomeMessage = "welcome to flatbook, Grace Hopper!";
+
+// let excitedWelcomeMessage = "WELCOME TO FLATBOOK,";
+// excitedWelcomeMessage = "WELCOME TO FLATBOOK, GARCE HOPPER";
+// excitedWelcomeMessage = "WELACOME TO FLATBOOK, GRACE HOPPER!";
+
+// let shortGreeting = "Welcome,";
+// shortGreeting = "Welcome, Grace";
+// shortGreeting = "Welcome, Grace!";
+
+const currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
